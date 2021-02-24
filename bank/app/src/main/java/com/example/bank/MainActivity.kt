7c0058dd271f1257bd2bbb7fd05713b26850ab97
irrem <50523883+irrem@users.noreply.github.com>
2021-02-25 00:02:52 +0300

    firebase login/logout işlemleri yapıldı

--- bank/app/src/main/java/com/example/bank/MainActivity.kt
+++ bank/app/src/main/java/com/example/bank/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.bank
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater= menuInflater
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.logout){
+            auth.signOut()
+            val intent= Intent(applicationContext,login::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //id "kotlin-android-extensions"
+        auth= FirebaseAuth.getInstance()
     }
 }
--- bank/app/src/main/java/com/example/bank/login.kt
+++ bank/app/src/main/java/com/example/bank/login.kt
@@ -1,11 +1,58 @@
 package com.example.bank
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class login : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    var email:String ?=null
+    var password:String?=null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth= FirebaseAuth.getInstance()
+
+        val currentUser= auth.currentUser
+        if(currentUser!=null){
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
+
+
+    fun loginOnClick(view: View){
+        email= emailTxt.text.toString()
+        password= findViewById<EditText>(R.id.password).text.toString()
+        //val emailText= emailTxt.text.toString()
+        if(email!=null && password!=null) {
+            auth.signInWithEmailAndPassword(email!!, password!!).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Giriş Başarılı!", Toast.LENGTH_LONG).show()
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(
+                    applicationContext,
+                    exception.localizedMessage.toString(),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }else{
+            Toast.makeText(applicationContext, "Şifre ve Mail kısımları boş bırakılamaz!", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
